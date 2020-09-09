cb5c581506a5eff1124a820215ea0127f5994c94
John Kim <j3k2@pm.me>
2020-09-09 15:01:02 -0700

    use whereIn query for cards to improve board fetch

--- server/queries/board.js
+++ server/queries/board.js
@@ -1,5 +1,6 @@
 const { create, find, update } = require('./common');
 const knex = require('../knex');
+const _ = require('lodash');
 
 async function createBoard(params) {
 	return create('boards', params, ['id', 'title']);
@@ -11,29 +12,16 @@ async function getBoards(params) {
 
 async function getBoard(params) {
 	const board = await knex('boards').where({ id: params.board_id }).first();
-	board.lanes = await knex('lanes').where({ board_id: params.board_id });
-	// const board = await knex('boards')
-	// 	.innerJoin('lanes', function () {
-	// 		this.on('boards.id', 'lanes.board_id');
-	// 		this.andOnVal('boards.id', '=', params.board_id)
-	// 	})
-	// 	.select([
-	// 		'boards.id as id',
-	// 		'boards.title as title',
-	// 		knex.raw(`ARRAY_AGG(json_build_object('id', lanes.id, 'title', lanes.title, 'order', lanes.order)) as lanes`)
-	// 	])
-	// 	.groupBy('boards.id')
-	// 	.first();
 
-	board.lanes = board.lanes.sort((a, b) => a.order - b.order);
+	board.lanes = await knex('lanes').where({ board_id: params.board_id }).orderBy('order');
 
-	const cardsPerLane = await Promise.all(board.lanes.map(async (lane) => {
-		return knex('cards').where({ lane_id: lane.id });
-	}));
-
-	board.lanes.forEach((lane, idx) => {
-		lane.cards = cardsPerLane[idx].sort((a, b) => a.order - b.order);
+	const laneIds = _.map(board.lanes, 'id');
+	const cards = await knex('cards').whereIn('lane_id', laneIds).orderBy('order');
+	const cardsByLane = _.groupBy(cards, 'lane_id');
+	board.lanes.forEach(lane => {
+		lane.cards = cardsByLane[lane.id];
 	});
+
 	return board;
 }
 
