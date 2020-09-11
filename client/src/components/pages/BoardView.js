e0f12f624856f81a3734398ae18780ec7804964e
John Kim <j3k2@pm.me>
2020-09-10 19:52:23 -0700

    BoardView state structure and error handling

--- client/src/components/pages/BoardView.js
+++ client/src/components/pages/BoardView.js
@@ -13,22 +13,15 @@ import { DragDropContext, Droppable } from 'react-beautiful-dnd';
 
 const Board = () => {
   const params = useParams();
-  const history = useHistory();
 
-  const [lanes, setLanes] = React.useState([]);
-
-  const [title, setTitle] = React.useState('');
+  const [board, setBoard] = React.useState(null);
 
   React.useEffect(() => {
-
     const fetchBoard = async () => {
       const board = await boardsService.getBoard(params.id);
       if (board) {
-        setTitle(board.title);
-        setLanes(board.lanes);
+        setBoard(board);
         document.title = `${board.title} | Cello`;
-      } else {
-        history.push('/');
       }
     }
     fetchBoard();
@@ -36,27 +29,26 @@ const Board = () => {
 
   const addLane = (lane) => {
     lane.cards = [];
-    setLanes([...lanes, lane])
+    setBoard({ ...board, lanes: [...board.lanes, lane] });
   }
 
   const addCard = (laneId, card) => {
-    const lanesCopy = [...lanes];
-    const lane = lanesCopy.find(lane => lane.id === laneId);
+    const lanes = [...board.lanes];
+    const lane = lanes.find(lane => lane.id === laneId);
     lane.cards = [...lane.cards, card];
-
-    setLanes(lanesCopy);
+    setBoard({ ...board, lanes });
   }
 
   const reorderLaneCards = (laneId, cardId, prevIdx, nextIdx) => {
-    const lanesCopy = [...lanes];
-    const lane = lanesCopy.find(lane => lane.id === laneId);
-    const cards = [...lane.cards]; // needed to prevent mutation
+    const lanes = [...board.lanes];
+    const lane = lanes.find(lane => lane.id === laneId);
+    const cards = [...lane.cards];
     const card = cards.find(card => card.id === cardId);
     cards.splice(prevIdx, 1);
     cards.splice(nextIdx, 0, card);
     lane.cards = cards;
 
-    setLanes(lanesCopy);
+    setBoard({ ...board, lanes });
     cardsService.moveCard(cardId, {
       next: nextIdx,
       prev: prevIdx,
@@ -65,10 +57,10 @@ const Board = () => {
   }
 
   const moveCardToLane = (cardId, prevLaneId, nextLaneId, prevIdx, nextIdx) => {
-    const lanesCopy = [...lanes];
-    const prevLane = lanesCopy.find(lane => lane.id === prevLaneId);
+    const lanes = [...board.lanes];
+    const prevLane = lanes.find(lane => lane.id === prevLaneId);
     const prevCards = [...prevLane.cards];
-    const nextLane = lanesCopy.find(lane => lane.id === nextLaneId);
+    const nextLane = lanes.find(lane => lane.id === nextLaneId);
     const nextCards = [...nextLane.cards];
 
     const card = prevCards.find(card => card.id === cardId);
@@ -77,7 +69,7 @@ const Board = () => {
     prevLane.cards = prevCards;
     nextLane.cards = nextCards;
 
-    setLanes(lanesCopy);
+    setBoard({ ...board, lanes });
     cardsService.moveCard(cardId, {
       next: nextIdx,
       prev: prevIdx,
@@ -87,12 +79,12 @@ const Board = () => {
   }
 
   const moveLane = (laneId, prevIdx, nextIdx) => {
-    const lanesCopy = [...lanes];
-    const lane = lanesCopy.find(lane => lane.id === laneId);
-    lanesCopy.splice(prevIdx, 1);
-    lanesCopy.splice(nextIdx, 0, lane);
+    const lanes = [...board.lanes];
+    const lane = lanes.find(lane => lane.id === laneId);
+    lanes.splice(prevIdx, 1);
+    lanes.splice(nextIdx, 0, lane);
 
-    setLanes(lanesCopy);
+    setBoard({ ...board, lanes });
     lanesService.moveLane(laneId, {
       next: nextIdx,
       prev: prevIdx,
@@ -101,10 +93,16 @@ const Board = () => {
   }
 
   const editLane = (id, edits) => {
-    const lanesCopy = [...lanes];
-    let lane = lanesCopy.find(lane => lane.id === id);
+    const lanes = [...board.lanes];
+    let lane = lanes.find(lane => lane.id === id);
     lane = Object.assign(lane, edits);
-    setLanes(lanesCopy);
+    setBoard({ ...board, lanes });
+  }
+
+  const editBoard = (updates) => {
+    const updatedBoard = Object.assign({}, board, updates);
+    setBoard(updatedBoard);
+    document.title = `${updatedBoard.title} | Cello`;
   }
 
   const onDragEnd = (result, provided) => {
@@ -134,24 +132,23 @@ const Board = () => {
   return (
     <div className="board-view-wrapper">
       <div className="board-view-header">
-        <div className="board-title">
-          <Editor content={title} updateContent={async (updatedTitle) => {
-            setTitle(updatedTitle);
+        {board && <div className="board-title">
+          <Editor content={board.title} updateContent={async (updatedTitle) => {
+            editBoard({ title: updatedTitle });
             const updatedFields = await boardsService.editBoard(params.id, { title: updatedTitle });
             if (updatedFields) {
-              setTitle(updatedFields.title);
-              document.title = `${updatedFields.title} | Cello`;
+              editBoard(updatedFields);
             }
           }} />
-        </div>
+        </div>}
       </div>
       <div className="board-view-container">
-        <DragDropContext onDragEnd={onDragEnd}>
-          <Droppable className="lane-droppable"
+        {board && <DragDropContext onDragEnd={onDragEnd}>
+          <Droppable
             type="LANE" droppableId="droppable.board" direction="horizontal">
             {(provided, snapshot) => (
               <div className="board-view-content" ref={provided.innerRef} {...provided.droppableProps}>
-                <LanesList lanes={lanes} addCard={addCard} editLane={editLane} />
+                <LanesList lanes={board.lanes} addCard={addCard} editLane={editLane} />
                 {provided.placeholder}
                 <div className="board-lane lane-creator">
                   <Creator
@@ -163,12 +160,13 @@ const Board = () => {
                     }}
                     placeholder={'Enter list title'}
                     buttonText={'Add list'}
-                    toggleText={lanes.length ? 'Add another list' : 'Add a list'} />
+                    toggleText={board.lanes.length ? 'Add another list' : 'Add a list'} />
                 </div>
               </div>
             )}
           </Droppable>
-        </DragDropContext>
+        </DragDropContext>}
+        {!board && <div>Board not found</div>}
       </div>
     </div>)
 }
