d3c354a1a525a3d2a3fb0be7c9ed43e2e13bdec3
John Kim <j3k2@pm.me>
2020-09-16 16:31:09 -0700

    fix lane dragging

--- client/src/components/lanes/LaneWrapper.js
+++ client/src/components/lanes/LaneWrapper.js
@@ -1,29 +1,31 @@
 import React from "react";
 
-function LaneWrapper({children}) {
-  return <div className="board-lane">
-    {children}
-    <style jsx>
-      {`
-        .board-lane {
-          margin: 0px 4px;
-          min-width: 272px;
-          max-width: 272px;
-          overflow-wrap: break-word;
-          display: flex;
-          flex-direction: column;
-          &:first-child {
-            margin-left: 8px;
+const LaneWrapper = React.forwardRef((props, ref) => {
+  return (
+    <div ref={ref} className="board-lane" {...props}>
+      {props.children}
+      <style jsx>
+        {`
+          .board-lane {
+            margin: 0px 4px;
+            min-width: 272px;
+            max-width: 272px;
+            overflow-wrap: break-word;
+            display: flex;
+            flex-direction: column;
+            &:first-child {
+              margin-left: 8px;
+            }
+            &:last-child {
+              min-width: 280px;
+              max-width: 280px;
+              border-right: solid 8px transparent;
+            }
           }
-          &:last-child {
-            min-width: 280px;
-            max-width: 280px;
-            border-right: solid 8px transparent;
-          }
-        }
-      `}
-    </style>
-  </div>;
-}
+        `}
+      </style>
+    </div>
+  );
+});
 
 export default LaneWrapper;
